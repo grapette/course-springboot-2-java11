5c0e149abaf4adc826fbc5195705e96eff126523
grapette <igorpompeuzs@yahoo.com.br>
2020-10-27 21:26:34 -0300

    Category

---
+++ src/main/java/com/igorp/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.igorp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.igorp.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+}
